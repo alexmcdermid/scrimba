79a5e546b24f644ded8d1f108c9c42028cc20bf5
Alex McDermid <alexander.mcdermid@alumni.ubc.ca>
2023-08-02 17:58:01 -0700

    second module

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -4,6 +4,7 @@ import Home from "../components/Home";
 import Modules from "../components/Modules";
 import ModuleOne from "./ModuleOne/ModuleOne";
 import DisplayPhotos from "./ModuleOne/DisplayPhotos";
+import ModuleTwo from "./ModuleTwo/ModuleTwo";
 
 const App = () => (
   <Router>
@@ -12,6 +13,7 @@ const App = () => (
       <Route path="/modules" element={<Modules />} />
       <Route path="/module/1" element={<ModuleOne />} />
       <Route path="/module/1/display-photos" element={<DisplayPhotos />} />
+      <Route path="/module/2" element={<ModuleTwo />} />
     </Routes>
   </Router>
 );
--- app/javascript/components/ModuleTwo/ModuleTwo.jsx
+++ app/javascript/components/ModuleTwo/ModuleTwo.jsx
@@ -1,25 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Module = () => (
+const ModuleTwo = () => (
   <div id="links">
     <div className="d-flex gap-2">
           <Link
-            to="/module/1"
+            to="/module/2/1"
             className="btn btn-sm custom-button"
             role="button"
           >
-            Module 1
-          </Link>
-          <Link
-            to="/module/2"
-            className="btn btn-sm custom-button"
-            role="button"
-          >
-            Module 2
+            1
           </Link>
         </div>
   </div>
 )
 
-export default Module
+export default ModuleTwo
