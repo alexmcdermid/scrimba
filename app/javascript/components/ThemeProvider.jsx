e30939e32f7228ca627f2c39bfac2c66a8fae29c
Alex McDermid <alexander.mcdermid@alumni.ubc.ca>
2023-08-03 22:48:11 -0700

    complete lightmode darkmode setup

--- app/javascript/components/ThemeProvider.jsx
+++ app/javascript/components/ThemeProvider.jsx
@@ -1,14 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { ThemeContext } from "../ThemeContext";
 
 const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState("dark");
+  const currentTheme = document.documentElement.getAttribute('data-bs-theme');
+  const [theme, setTheme] = useState(currentTheme);
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-bs-theme', theme);
+  }, [theme]);
 
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
       {children}
     </ThemeContext.Provider>
   );
-}
+};
 
 export default ThemeProvider;
