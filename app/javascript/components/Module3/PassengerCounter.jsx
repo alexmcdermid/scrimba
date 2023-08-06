c02c56e42877fef5cf1e05ac9d496900c1fa76e4
alexmcdermid <alexander.mcdermid@alumni.ubc.ca>
2023-08-06 12:37:02 -0700

    decrement and very basic styling

--- app/javascript/components/Module3/PassengerCounter.jsx
+++ app/javascript/components/Module3/PassengerCounter.jsx
@@ -4,17 +4,25 @@ export default class PassengerCounter extends Component {
   componentDidMount() {
     const peopleCount = document.getElementById("people-count-el")
     const incrementButton = document.getElementById("increment-btn")
+    const decrementButton = document.getElementById("decrement-btn")
     incrementButton.addEventListener("click", () => {
       peopleCount.innerText = Number(peopleCount.innerText) + 1
+    })
+    decrementButton.addEventListener("click", () => {
+      peopleCount.innerText = Number(peopleCount.innerText) - 1
     })
   }
 
   render() {
     return (
-      <div>
-        <h1>People entered: </h1>
-        <h2 id="people-count-el">0</h2>
-        <button id="increment-btn">Increment</button>
+      <div class="container">
+        <div class="d-flex flex-row justify-content-center">
+          <h1>People entered: <span id="people-count-el">0</span></h1>
+        </div>
+        <div class="d-flex flex-row justify-content-center">
+          <button id="increment-btn" class="btn btn-primary">Increment</button>
+          <button id="decrement-btn" class="btn btn-danger">Decrement</button>
+        </div>
       </div>
     )
   }
