fb7219499797184ea746fd34c69ef47dd8b86b88
alexmcdermid <alexander.mcdermid@alumni.ubc.ca>
2023-08-06 12:44:19 -0700

    save button for count history

--- app/javascript/components/Module3/PassengerCounter.jsx
+++ app/javascript/components/Module3/PassengerCounter.jsx
@@ -3,13 +3,18 @@ import React, { Component } from 'react'
 export default class PassengerCounter extends Component {  
   componentDidMount() {
     const peopleCount = document.getElementById("people-count-el")
+    const savedContent = document.getElementById("saved-content-el")
     const incrementButton = document.getElementById("increment-btn")
     const decrementButton = document.getElementById("decrement-btn")
+    const saveButton = document.getElementById("save-btn")
     incrementButton.addEventListener("click", () => {
       peopleCount.innerText = Number(peopleCount.innerText) + 1
     })
     decrementButton.addEventListener("click", () => {
       peopleCount.innerText = Number(peopleCount.innerText) - 1
+    })
+    saveButton.addEventListener("click", () => {
+      savedContent.innerText = savedContent.innerText + "..." + peopleCount.innerText
     })
   }
 
@@ -20,8 +25,14 @@ export default class PassengerCounter extends Component {
           <h1>People entered: <span id="people-count-el">0</span></h1>
         </div>
         <div class="d-flex flex-row justify-content-center">
-          <button id="increment-btn" class="btn btn-primary">Increment</button>
           <button id="decrement-btn" class="btn btn-danger">Decrement</button>
+          <button id="increment-btn" class="btn btn-primary">Increment</button>
+        </div>
+        <div class="d-flex flex-row justify-content-center">
+          <button id="save-btn" class="btn btn-success">Save</button>
+        </div>
+        <div class="d-flex flex-row justify-content-center">
+          <h2 id="saved-content-el">0</h2>
         </div>
       </div>
     )
