7ccaec75c2d8359018fb89aee84f72538ff6547d
Alex McDermid <alexander.mcdermid@alumni.ubc.ca>
2023-08-14 15:25:01 -0700

    factories and gists

--- app/javascript/components/Blog/RailsTestingBlog.jsx
+++ app/javascript/components/Blog/RailsTestingBlog.jsx
@@ -28,7 +28,26 @@ const RailsTestingBlog = () => {
       </p>
       <h2>Factories</h2>
       <p>
-
+        FactoryBot is a library for setting up Ruby objects as test data. Unlike fixtures, which provide static predefined data, FactoryBot offers a more dynamic and flexible approach. It uses the concept of factories to define the blueprint of objects and allows you to easily override, extend, or randomize the generated data.
+      </p>
+      <p>
+        Factories defined with FactoryBot are usually more adaptable and concise than fixtures. They can be easily combined, extended, and customized to fit different scenarios. This is particularly useful in tests where data needs to respect certain validations, or when you want to simulate various scenarios without having to redefine the entire data set.
+      </p>
+      <p>
+        In the context of RSpec, FactoryBot often feels more natural as it follows the same BDD principles. Instead of setting up a static state for your tests like fixtures do, FactoryBot generates the data you need on-demand, making it clear within the test what kind of data is being tested.
+      </p>
+      <p>
+        Here's an example of a factory for the User model:
+      </p>
+      <Gist id="8571673b3b6eea204ab3a49b45e0e9d6" />
+      <p>
+        With this factory, in an RSpec test, you can create a user like so:
+      </p>
+      <Gist id="6dfbf74d474b290cf2bd7712d796f610" />
+      <h2>Pros and Cons</h2>
+      <h2>Summary</h2>
+      <p>
+        In summary, while RSpec provides the structure and DSL for defining and running tests, both fixtures and FactoryBot factories offer ways to manage test data. Fixtures provide static, predefined data while FactoryBot offers a more dynamic and customizable approach, making it easier to set up varied scenarios within tests.
       </p>
     </div>
   )
