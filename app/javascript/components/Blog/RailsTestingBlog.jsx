8a0e77b51fd6b136ba214f9262bcda36b397fc63
Alex McDermid <alexander.mcdermid@alumni.ubc.ca>
2023-08-14 14:26:01 -0700

    react testing blog

--- app/javascript/components/Blog/RailsTestingBlog.jsx
+++ app/javascript/components/Blog/RailsTestingBlog.jsx
@@ -3,7 +3,19 @@ import React from 'react'
 const RailsTestingBlog = () => {
   return (
     <div className='container'>
-      <h1>Rails Testing; The battle of Fixtures vs FactoryBot</h1>
+      <h1>Rails Testing; The Battle of Factories vs Fixtures when using Rspec</h1>
+      <h2>Rspec</h2>
+      <p>
+
+      </p>
+      <h2>Factories</h2>
+      <p>
+
+      </p>
+      <h2>Fixtures</h2>
+      <p>
+        
+      </p>
     </div>
   )
 }
