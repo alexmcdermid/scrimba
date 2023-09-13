aa321343f361a3dc2bc66e94b6c0ec6f387b7427
Alex McDermid <alexander.mcdermid@alumni.ubc.ca>
2023-09-13 12:02:46 -0700

    part 2

--- app/javascript/components/Blog/AhoyOptimizationBlog.jsx
+++ app/javascript/components/Blog/AhoyOptimizationBlog.jsx
@@ -32,6 +32,23 @@ const AhoyOptimizationBlog = () => {
       <p>So, now, you've set up Ahoy in your Rails application and you're capturing rich, insightful data on user behavior. That's great—but what happens when the user base grows, and along with it, your Ahoy data tables? As you scale, performance issues may start to creep in, affecting the overall performance of your application.</p>
       <p>This is where Ahoy optimization strategies come into play. If you're committed to maintaining a high-performing application while still leveraging the full power of Ahoy, then the following sections are designed specifically for you. From smart database scaling strategies to application-level improvements and infrastructure tweaks, this read will highlight proven ways to keep your Ahoy Events and Visits as speedy as they are insightful.</p>
       <h2>Optimizing Ahoy at the Application Level</h2>
+      <p>Before diving into the deep waters of database optimization and infrastructure scaling, it's often beneficial to start with the codebase itself. This section will guide you through application-level strategies aimed at ensuring that your Ahoy events and visits tracking is efficient right from the start.</p>
+      <h3>Rate Limiting</h3>
+      <p>What: Implementing controls to restrict the rate of requests from clients.</p>
+      <p>Why: To prevent accidental or intentional heavy usage that could bring down the application.</p>
+      <p>How: Use middleware like Rack::Attack in your Rails application.</p>
+      <h3>Batching</h3>
+      <p>What: Writing multiple events to the database in a single transaction.</p>
+      <p>Why: Reduces the overall database resources needed for writing.</p>
+      <p>How: Use ActiveRecord’s import method or database-specific batch operations.</p>
+      <h3>Asynchronous Processing</h3>
+      <p>What: Offloading heavy lifting tasks to background jobs.</p>
+      <p>Why: Frees up the main thread, improving the overall performance of the application.</p>
+      <p>How: Utilize background job frameworks like Sidekiq or Delayed Job.</p>
+      <h3>Optimize Queries</h3>
+      <p>What: Fine-tuning SQL queries.</p>
+      <p>Why: Even minor adjustments can yield substantial performance gains.</p>
+      <p>How: Use tools like the bullet gem to identify N+1 queries and other inefficiencies.</p>
       <h2>Optimizing Ahoy at the Database Level</h2>
       <h2>Optimizing at the Infrastructure Level</h2>
       <h2>Setting the Stage for Optimized Performance</h2>
